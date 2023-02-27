d46638cd6064f71b8c7a351c327c42c263cdeae2
LuisSouza01 <luis.f.souza@outlook.com>
2023-02-26 21:28:45 -0300

    feat: render contacts at homePage

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 import edit from '../../assets/images/icons/Edit.svg';
 import trash from '../../assets/images/icons/Trash.svg';
@@ -8,47 +9,75 @@ import {
   InputSearchContainer, Header, ListContainer, Card,
 } from './styles';
 
-const Home = () => (
-  <>
-    <InputSearchContainer>
-      <input type="text" placeholder="Pesquisar contato" />
-    </InputSearchContainer>
+type Contact = {
+  id: String;
+  name: String;
+  phone: String;
+  email: String;
+  category_id: String;
+  category_name: String;
+}
 
-    <Header>
-      <strong>3 contatos</strong>
-      <Link to="/new">Novo contato</Link>
-    </Header>
+const Home = () => {
+  const [contacts, setContacts] = useState<Contact[]>([]);
 
-    <ListContainer>
-      <header>
-        <button type="button">
-          <span>Nome</span>
-          <img src={arrow} alt="Ícone de uma seta, em roxo" />
-        </button>
-      </header>
+  useEffect(() => {
+    fetch('http://localhost:3001/contacts')
+      .then((response) => response.json())
+      .then((data) => setContacts(data));
+  }, []);
 
-      <Card>
-        <div className="info">
-          <div className="contact-name">
-            <strong>Luis Souza</strong>
-            <small>Levva</small>
-          </div>
+  return (
+    <>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato" />
+      </InputSearchContainer>
 
-          <span>luis.souza@levva.io</span>
-          <span>(41) 99805-7276</span>
-        </div>
+      <Header>
+        <strong>
+          {contacts.length}
+          {contacts.length === 1 ? ' contato' : ' contatos'}
+        </strong>
+        <Link to="/new">Novo contato</Link>
+      </Header>
 
-        <div className="actions">
-          <Link to="/edit/123">
-            <img src={edit} alt="Ícone de uma caneta em cima de um papel, em azul" />
-          </Link>
-          <button type="button">
-            <img src={trash} alt="Ícone de uma lixeira, em vermelho" />
-          </button>
-        </div>
-      </Card>
-    </ListContainer>
-  </>
-);
+      {contacts.length > 0 && (
+        <ListContainer>
+          <header>
+            <button type="button">
+              <span>Nome</span>
+              <img src={arrow} alt="Ícone de uma seta, em roxo" />
+            </button>
+          </header>
+
+          {contacts.map((contact) => (
+            <Card>
+              <div className="info">
+                <div className="contact-name">
+                  <strong>{contact.name}</strong>
+                  {contact.category_name && (
+                    <small>{contact.category_name}</small>
+                  )}
+                </div>
+
+                <span>{contact.email}</span>
+                <span>{contact.phone}</span>
+              </div>
+
+              <div className="actions">
+                <Link to={`/edit/${contact.id}`}>
+                  <img src={edit} alt="Ícone de uma caneta em cima de um papel, em azul" />
+                </Link>
+                <button type="button">
+                  <img src={trash} alt="Ícone de uma lixeira, em vermelho" />
+                </button>
+              </div>
+            </Card>
+          ))}
+        </ListContainer>
+      )}
+    </>
+  );
+};
 
 export default Home;
