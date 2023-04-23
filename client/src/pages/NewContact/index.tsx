2e2528227485769df1b82c79e3e09eeb4b247b31
LuisSouza01 <luis.f.souza@outlook.com>
2023-04-23 06:03:37 -0300

    refact: changed newContact page logic to a customHook on client side

--- client/src/pages/NewContact/index.tsx
+++ client/src/pages/NewContact/index.tsx
@@ -1,41 +1,13 @@
-import { useRef } from 'react';
-
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
-import ContactsService from '../../services/ContactsService';
 
-import toast from '../../utils/toast';
-
-export interface NewContactFormData {
-  id: string;
-  name: string;
-  email: string;
-  phone: string;
-  categoryId: string;
-}
+import useNewContact from './useNewContact';
 
 const NewContact = () => {
-  const ContactFormRef = useRef<any>(null);
-
-  async function handleSubmit(contact: NewContactFormData) {
-    try {
-      await ContactsService.createContact(contact);
-
-      ContactFormRef.current.resetFields();
-
-      toast({
-        type: 'success',
-        text: 'Contato criado com sucesso!',
-        duration: 3000,
-      });
-    } catch {
-      toast({
-        type: 'danger',
-        text: 'Ocorreu um erro ao cadastrar o contato',
-        duration: 3000,
-      });
-    }
-  }
+  const {
+    ContactFormRef,
+    handleSubmit,
+  } = useNewContact();
 
   return (
     <>
---
+++ client/src/pages/NewContact/useNewContact.ts
@@ -0,0 +1,43 @@
+import { useRef } from 'react';
+
+import toast from '../../utils/toast';
+import ContactsService from '../../services/ContactsService';
+
+export interface NewContactFormData {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  categoryId: string;
+}
+
+const useNewContact = () => {
+  const ContactFormRef = useRef<any>(null);
+
+  async function handleSubmit(contact: NewContactFormData) {
+    try {
+      await ContactsService.createContact(contact);
+
+      ContactFormRef.current.resetFields();
+
+      toast({
+        type: 'success',
+        text: 'Contato criado com sucesso!',
+        duration: 3000,
+      });
+    } catch {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao cadastrar o contato',
+        duration: 3000,
+      });
+    }
+  }
+
+  return {
+    ContactFormRef,
+    handleSubmit,
+  };
+};
+
+export default useNewContact;
