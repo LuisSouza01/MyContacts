c904a64f82bc562fc03a1f0c7d1b6bbe302ffef4
LuisSouza01 <luis.f.souza@outlook.com>
2023-04-26 12:02:27 -0300

    refact: created ContactsList component at homepage on client side

---
+++ client/src/pages/Home/components/ContactsList/index.tsx
@@ -0,0 +1,87 @@
+/* eslint-disable no-unused-vars */
+import { Link } from 'react-router-dom';
+
+import edit from '../../assets/images/icons/Edit.svg';
+import trash from '../../assets/images/icons/Trash.svg';
+import arrow from '../../assets/images/icons/Arrow.svg';
+
+import Modal from '../../../../components/Modal';
+import { ContactDomainMapper } from '../../../../services/mappers/ContactMapper';
+
+import {
+  Card,
+  ListHeader,
+} from './styles';
+
+type ContactsListProps = {
+  orderBy: string;
+  onToogleOrderBy: () => void;
+  filteredContacts: ContactDomainMapper[];
+  onDeleteContact: (contact: ContactDomainMapper) => void;
+  contactBeingDeleted: ContactDomainMapper | undefined;
+  onCloseDeleteContact: () => void;
+  onConfirmDeleteContact: () => void;
+  isDeleteModalVisible: boolean;
+}
+
+const ContactsList = ({
+  orderBy,
+  onToogleOrderBy,
+  filteredContacts,
+  onDeleteContact,
+  contactBeingDeleted,
+  onCloseDeleteContact,
+  onConfirmDeleteContact,
+  isDeleteModalVisible,
+}: ContactsListProps) => (
+  <>
+    <ListHeader orderBy={orderBy}>
+      <button type="button" onClick={onToogleOrderBy}>
+        <span>Nome</span>
+        <img src={arrow} alt="Ícone de uma seta, em roxo" />
+      </button>
+    </ListHeader>
+
+    {filteredContacts.map((contact) => (
+      <Card key={contact.id}>
+        <div className="info">
+          <div className="contact-name">
+            <strong>{contact.name}</strong>
+            {contact.category.name && (
+            <small>{contact.category.name}</small>
+            )}
+          </div>
+
+          <span>{contact.email}</span>
+          <span>{contact.phone}</span>
+        </div>
+
+        <div className="actions">
+          <Link to={`/edit/${contact.id}`}>
+            <img src={edit} alt="Ícone de uma caneta em cima de um papel, em azul" />
+          </Link>
+
+          <button
+            type="button"
+            onClick={() => onDeleteContact(contact)}
+          >
+            <img src={trash} alt="Ícone de uma lixeira, em vermelho" />
+          </button>
+        </div>
+      </Card>
+    ))}
+
+    <Modal
+      danger
+      title={`Tem certeza que deseja remover o contato "${contactBeingDeleted?.name}"?`}
+      confirmLabel="Deletar"
+      onCancel={onCloseDeleteContact}
+      onConfirm={onConfirmDeleteContact}
+      visible={isDeleteModalVisible}
+    >
+      <p>Esta ação não poderá ser desfeita!</p>
+    </Modal>
+  </>
+);
+
+export default ContactsList;
--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,23 +1,12 @@
-import { Link } from 'react-router-dom';
-
 import useHome from './useHome';
 
-import edit from '../../assets/images/icons/Edit.svg';
-import trash from '../../assets/images/icons/Trash.svg';
-import arrow from '../../assets/images/icons/Arrow.svg';
-
 import Header from './components/Header';
-import Modal from '../../components/Modal';
 import Loader from '../../components/Loader';
 import InputSearch from './components/InputSearch';
 import ErrorStatus from './components/ErrorStatus';
 import EmptyList from './components/EmptyList';
 import SearchNotFound from './components/SearchNotFound';
-
-import {
-  ListHeader,
-  Card,
-} from './styles';
+import ContactsList from './components/ContactsList';
 
 const Home = () => {
   const {
@@ -69,57 +58,20 @@ const Home = () => {
           )}
 
           {filteredContacts.length > 0 && (
-            <>
-              <ListHeader orderBy={orderBy}>
-                <button type="button" onClick={handleToogleOrderBy}>
-                  <span>Nome</span>
-                  <img src={arrow} alt="Ícone de uma seta, em roxo" />
-                </button>
-              </ListHeader>
-
-              {filteredContacts.map((contact) => (
-                <Card key={contact.id}>
-                  <div className="info">
-                    <div className="contact-name">
-                      <strong>{contact.name}</strong>
-                      {contact.category.name && (
-                        <small>{contact.category.name}</small>
-                      )}
-                    </div>
-
-                    <span>{contact.email}</span>
-                    <span>{contact.phone}</span>
-                  </div>
-
-                  <div className="actions">
-                    <Link to={`/edit/${contact.id}`}>
-                      <img src={edit} alt="Ícone de uma caneta em cima de um papel, em azul" />
-                    </Link>
-
-                    <button
-                      type="button"
-                      onClick={() => handleDeleteContact(contact)}
-                    >
-                      <img src={trash} alt="Ícone de uma lixeira, em vermelho" />
-                    </button>
-                  </div>
-                </Card>
-              ))}
-            </>
+            <ContactsList
+              orderBy={orderBy}
+              filteredContacts={filteredContacts}
+              onDeleteContact={handleDeleteContact}
+              onToogleOrderBy={handleToogleOrderBy}
+              contactBeingDeleted={contactBeingDeleted}
+              isDeleteModalVisible={isDeleteModalVisible}
+              onCloseDeleteContact={handleCloseDeleteContact}
+              onConfirmDeleteContact={handleConfirmDeleteContact}
+            />
           )}
         </>
       )}
 
-      <Modal
-        danger
-        title={`Tem certeza que deseja remover o contato "${contactBeingDeleted?.name}"?`}
-        confirmLabel="Deletar"
-        onCancel={handleCloseDeleteContact}
-        onConfirm={handleConfirmDeleteContact}
-        visible={isDeleteModalVisible}
-      >
-        <p>Esta ação não poderá ser desfeita!</p>
-      </Modal>
     </>
   );
 };
