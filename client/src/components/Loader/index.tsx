07b78613747e26994cf2e5e087e7803a075af68b
LuisSouza01 <luis.f.souza@outlook.com>
2023-05-02 18:04:37 -0300

    feat: created animation fadeIn and fadeOut at Loader component on client side

--- client/src/components/Loader/index.tsx
+++ client/src/components/Loader/index.tsx
@@ -2,19 +2,22 @@ import { Overlay } from './styles';
 
 import Spinner from '../Spinner';
 import ReactPortal from '../ReactPortal';
+import useAnimatedUnmount from '../../hooks/useAnimatedUnmount';
 
 type LoaderProps = {
   isLoading: boolean;
 }
 
 const Loader = ({ isLoading }: LoaderProps) => {
-  if (!isLoading) {
+  const { shouldRender, animatedElementRef } = useAnimatedUnmount(isLoading);
+
+  if (!shouldRender) {
     return null;
   }
 
   return (
     <ReactPortal containerId="loader-root">
-      <Overlay>
+      <Overlay isLeaving={!isLoading} ref={animatedElementRef}>
         <Spinner size={90} />
       </Overlay>
     </ReactPortal>
--- client/src/components/Loader/styles.ts
+++ client/src/components/Loader/styles.ts
@@ -1,6 +1,30 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
-export const Overlay = styled.div`
+type OverlayProps = {
+  isLeaving: boolean;
+}
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`;
+
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+  }
+
+  to {
+    opacity: 0;
+  }
+`;
+
+export const Overlay = styled.div<OverlayProps>`
   width: 100%;
   height: 100%;
   background: rgba(246, 245, 252, 0.7);
@@ -12,4 +36,9 @@ export const Overlay = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  animation: ${fadeIn} 0.3s;
+
+  ${({ isLeaving }) => isLeaving && css`
+    animation: ${fadeOut} 0.3s forwards;
+  `}
 `;
